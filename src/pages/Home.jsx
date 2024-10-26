117c2337d524fa5460bb85140c36f71e7b6c4633
staRed1 <joaoarthur.prates@gmail.com>
2024-10-26 15:38:51 -0400

    update 1
    ddd

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function Home() {
+
+    const [lista, setLista] = useState([]);
+
+    useEffect(() => {
+        const receberListaProdutos = async () => {
+            try{
+                const resposta = await fetch('https://fakestoreapi.com/products');    
+            } catch (erro) {
+                alert("A conecxão com a API falhou");
+            }
+        }
+
+        receberListaProdutos();
+    }, []);
+
+    return (
+        <>
+        <h1>Lista Produtos</h1>
+        <ul>
+            {produto.map(produto => (
+                <li key={produto.id}>
+                <h2>{produto.title}</h2>
+                <p>{produto.description}</p>
+                <p>Preço: R${produto.price}</p>
+                <img src={produto.image} alt={produto.title} width={100}/>
+            </li>
+            ))}  
+            </ul>
+        </>
+    );
+}
